a1e53e88147dfbb6f6dc594cd47711b3e117b18d
AbuHanifSiam <siamabuhanif1@gmail.com>
2023-11-24 23:04:41 +0600

    update the mouse pointer

--- Assignment 3/Assignment3.py
+++ Assignment 3/Assignment3.py
@@ -4,6 +4,8 @@ from OpenGL.GLU import *
 import time
 
 radius = 0
+xc = 250
+yc = 250
 def draw_circle(radius, xc, yc):
     glBegin(GL_POINTS)
     x = 0
@@ -37,8 +39,19 @@ def iterate():
     glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0) #multiply the current matrix with an orthographic matrix and replaces the current matrix with the result
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
-
+    
+    
+def mouse_click(button, state, x, y):
+    global radius, xc, yc        
+    if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
+        xc = x
+        yc = 500 - y
+        glutDisplayFunc(showScreen)
+        glutPostRedisplay()
+        
+        
 def showScreen():
+    global radius, xc, yc
     glClearColor(1.0, 1.0, 1.0, 1.0) #set the background color to white. RGB and Alpha
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
@@ -46,7 +59,7 @@ def showScreen():
     iterate()
     glColor3f(0.0, 0.0, 0.0) #konokichur color set (RGB)
     #call the draw methods here
-    draw_circle(radius, 250, 250)
+    draw_circle(radius, xc, yc)
     glutSwapBuffers()
 
 
@@ -67,6 +80,7 @@ glutInitWindowSize(500, 500) #window size
 glutInitWindowPosition(0, 0) #window position in computer display, for this code it will be top left corner
 wind = glutCreateWindow(b"OpenGL Coding Practice") #window name
 glutDisplayFunc(showScreen)
+glutMouseFunc(mouse_click)
 glutIdleFunc(animate)
 glutMainLoop()
 
